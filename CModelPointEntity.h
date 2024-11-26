2d012719463832b1e1adf53c7facabe41d6291e4
Hasanovsky <58742759+Hasanovsky@users.noreply.github.com>
2024-11-26 21:42:39 +0400

    Add files via upload

---
+++ CModelPointEntity.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "CBaseEntity.h"
+
+class CModelPointEntity : public CBaseEntity
+{
+public:
+	DECLARE_SCHEMA_CLASS(CModelPointEntity)
+
+};
---
+++ CPointWorldText.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "CModelPointEntity.h"
+
+class CPointWorldText : public CModelPointEntity
+{
+public:
+	DECLARE_SCHEMA_CLASS(CPointWorldText)
+
+	SCHEMA_FIELD(CUtlSymbolLarge, m_messageText)
+	SCHEMA_FIELD(CUtlSymbolLarge, m_FontName)
+	SCHEMA_FIELD(CUtlSymbolLarge, m_BackgroundMaterialName)
+	SCHEMA_FIELD(bool, m_bEnabled)
+	SCHEMA_FIELD(bool, m_bFullbright)
+	SCHEMA_FIELD(float32, m_flWorldUnitsPerPx)
+	SCHEMA_FIELD(float32, m_flFontSize)
+	SCHEMA_FIELD(float32, m_flDepthOffset)
+	SCHEMA_FIELD(bool, m_bDrawBackground)
+	SCHEMA_FIELD_POINTER(Color, m_Color)
+	//float32 m_flBackgroundBorderWidth;
+	//float32 m_flBackgroundBorderHeight;
+	//float32 m_flBackgroundWorldToUV;
+};
