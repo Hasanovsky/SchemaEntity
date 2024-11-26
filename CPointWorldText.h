71758a859a752d73e64b8ac78fcc7d7e4d2088ef
Hasanovsky <58742759+Hasanovsky@users.noreply.github.com>
2024-11-26 23:13:01 +0400

    Update CPointWorldText.h

--- CPointWorldText.h
+++ CPointWorldText.h
@@ -6,8 +6,8 @@ class CPointWorldText : public CModelPointEntity
 public:
 	DECLARE_SCHEMA_CLASS(CPointWorldText)
 
-	SCHEMA_FIELD(char[512], m_messageText)
-	SCHEMA_FIELD(CUtlSymbolLarge, m_FontName)
+	SCHEMA_FIELD_OFFSET(char *, m_messageText, 0)
+	SCHEMA_FIELD_OFFSET(char *, m_FontName, 0)
 	SCHEMA_FIELD(CUtlSymbolLarge, m_BackgroundMaterialName)
 	SCHEMA_FIELD(bool, m_bEnabled)
 	SCHEMA_FIELD(bool, m_bFullbright)
