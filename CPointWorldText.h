2f6cb041cebd2b4a6187a6e447119779d6a052f7
Hasanovsky <58742759+Hasanovsky@users.noreply.github.com>
2024-11-27 00:49:42 +0400

    Update CPointWorldText.h

--- CPointWorldText.h
+++ CPointWorldText.h
@@ -1,6 +1,18 @@
 #pragma once
 #include "CModelPointEntity.h"
 
+enum class PointWorldTextJustifyHorizontal_t : uint32_t {
+	POINT_WORLD_TEXT_JUSTIFY_HORIZONTAL_LEFT = 0x0,
+	POINT_WORLD_TEXT_JUSTIFY_HORIZONTAL_CENTER = 0x1,
+	POINT_WORLD_TEXT_JUSTIFY_HORIZONTAL_RIGHT = 0x2,
+};
+
+enum class PointWorldTextJustifyVertical_t : uint32_t {
+	POINT_WORLD_TEXT_JUSTIFY_VERTICAL_BOTTOM = 0x0,
+	POINT_WORLD_TEXT_JUSTIFY_VERTICAL_CENTER = 0x1,
+	POINT_WORLD_TEXT_JUSTIFY_VERTICAL_TOP = 0x2,
+};
+
 class CPointWorldText : public CModelPointEntity
 {
 public:
@@ -16,6 +28,8 @@ public:
 	SCHEMA_FIELD(float32, m_flDepthOffset)
 	SCHEMA_FIELD(bool, m_bDrawBackground)
 	SCHEMA_FIELD_POINTER(Color, m_Color)
+	SCHEMA_FIELD(PointWorldTextJustifyHorizontal_t, m_nJustifyHorizontal)
+	SCHEMA_FIELD(PointWorldTextJustifyVertical_t, m_nJustifyVertical)
 	//float32 m_flBackgroundBorderWidth;
 	//float32 m_flBackgroundBorderHeight;
 	//float32 m_flBackgroundWorldToUV;
