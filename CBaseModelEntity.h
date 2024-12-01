9f0d0359ba299ca15292e11cc2b606824adf016b
Hasanovsky <58742759+Hasanovsky@users.noreply.github.com>
2024-12-01 09:39:47 +0400

    Update CBaseModelEntity.h

--- CBaseModelEntity.h
+++ CBaseModelEntity.h
@@ -2,23 +2,6 @@
 #include "CBaseEntity.h"
 #include "globaltypes.h"
 
-enum class RenderMode_t : uint8_t
-{
-	kRenderNormal = 0x0,
-	kRenderTransColor = 0x1,
-	kRenderTransTexture = 0x2,
-	kRenderGlow = 0x3,
-	kRenderTransAlpha = 0x4,
-	kRenderTransAdd = 0x5,
-	kRenderEnvironmental = 0x6,
-	kRenderTransAddFrameBlend = 0x7,
-	kRenderTransAlphaAdd = 0x8,
-	kRenderWorldGlow = 0x9,
-	kRenderNone = 0xa,
-	kRenderDevVisualizer = 0xb,
-	kRenderModeCount = 0xc,
-};
-
 class CBaseModelEntity : public CBaseEntity
 {
 public:
