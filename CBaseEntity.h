cd552969e0fdc9cd2ad7ed03b4afb8919b0d01ec
Hasanovsky <58742759+Hasanovsky@users.noreply.github.com>
2024-12-17 16:11:07 +0400

    Update CBaseEntity.h

--- CBaseEntity.h
+++ CBaseEntity.h
@@ -172,6 +172,7 @@ public:
 	Vector GetVecOrigin() { return m_CBodyComponent->m_pSceneNode->m_vecOrigin; }
 	Vector GetAbsOrigin() { return m_CBodyComponent->m_pSceneNode->m_vecAbsOrigin; }
 	QAngle GetAbsRotation() { return m_CBodyComponent->m_pSceneNode->m_angAbsRotation; }
+	QAngle GetAngRotation() { return m_CBodyComponent->m_pSceneNode->m_angRotation; }
 	Vector GetAbsVelocity() { return m_vecAbsVelocity; }
 	void SetAbsOrigin(Vector vecOrigin) { m_CBodyComponent->m_pSceneNode->m_vecAbsOrigin = vecOrigin; }
 	void SetAbsRotation(QAngle angAbsRotation) { m_CBodyComponent->m_pSceneNode->m_angAbsRotation = angAbsRotation; }
